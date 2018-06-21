5935c83eaed1ac52cbc6be8bdf664fd594b1f9c6
Nikki S <nikkisharpley@gmail.com>
2018-06-21 15:45:34 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var express     = require("express"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://nikkis:sbb729@ds151970.mlab.com:51970/nikkisyelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
